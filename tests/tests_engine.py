d9ba93d70537b9c46a965bed14730fc90b992d01
kaediin <skaedin@gmail.com>
2021-06-10 16:56:45 +0200

    Argument code Sprint 1 #10 Added docstrings and argument for the engine unittests

--- tests/tests_engine.py
+++ tests/tests_engine.py
@@ -5,6 +5,7 @@ from models import *
 
 
 def get_random_user_engine():
+    # Create the engine class and return it
     dataset = get_dataset(['../data/dataset_2017.csv'])
     user, old_user = dataset.get_random_user()
     similar = dataset.get_similar_candidates(user)
@@ -15,10 +16,12 @@ def get_random_user_engine():
 class TestCases(unittest.TestCase):
 
     def test_user_has_traits(self):
+        # check if the function 'get_scored_traits' works
         engine, user = get_random_user_engine()
         self.assertIsNotNone(engine.get_scored_traits())
 
     def test_no_crash_computations(self):
+        # try to run the function 'compute_predictions'. If there is no error thrown, it works
         engine, user = get_random_user_engine()
         try:
             compute_predictions(user, engine)
