6a3343e2111e3b8914aa9bae2733ee10c26f8418
kaediin <skaedin@gmail.com>
2021-06-10 10:11:47 +0200

    Write unittests for algorithms implemented in Sprint 1 #12 Made a test for algorithm-computations

---
+++ tests/tests_algorithms.py
@@ -0,0 +1,33 @@
+import unittest
+
+from main import *
+from models import *
+
+
+class TestCases(unittest.TestCase):
+
+    def test_user_has_similar(self):
+        dataset = get_dataset(['../data/dataset_2017.csv'])
+        user, old_user = dataset.get_random_user()
+        similars = []
+        threshold = 1
+        while len(similars) == 0:
+            similars = dataset.get_similar_candidates(user, threshold=threshold)
+            threshold += 1
+            if threshold >= 5:
+                self.fail(f'User {user} has no similar candidates!')
+        self.assertGreaterEqual(len(similars), 1)
+
+    def test_no_crash_computations(self):
+        dataset = get_dataset(['../data/dataset_2017.csv'])
+        user, old_user = dataset.get_random_user()
+        similars = dataset.get_similar_candidates(user)
+        engine = Engine(user=user, candidates=similars)
+        try:
+            compute_predictions(user, engine)
+        except (IndexError, ValueError) as e:
+            self.fail(f"Function raised exception: {e.args}")
+
+
+if __name__ == '__main__':
+    unittest.main()
