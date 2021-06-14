70b9b38376ca9de696240c5c5e96f7e3d8d285ea
kaediin <skaedin@gmail.com>
2021-06-14 13:32:16 +0200

    Research, Code and document KNN-algorithm #15 created testcases for KNN-algorithm. (not the unittests). These are graphs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from models import *
 from utils import *
+import timeit
 import copy as copy
 import matplotlib.pyplot as plot
 
@@ -8,6 +9,11 @@ def compute_predictions(user, engine):
     empty_traits = get_empty_traits(user)
     user_mode = copy.deepcopy(user)
     user_mean = copy.deepcopy(user)
+    user_knn = copy.deepcopy(user)
+
+    prediction_knn = engine.predict_scores_knn(empty_traits)
+    for row in prediction_knn:
+        user_knn.scores.__dict__[row[0]] = row[1]
 
     for trait in empty_traits:
         prediction_mode, confidence = engine.calculate_score_upcf(trait)
@@ -15,7 +21,7 @@ def compute_predictions(user, engine):
         user_mode.scores.__dict__[trait] = prediction_mode
         user_mean.scores.__dict__[trait] = str(round(prediction_mean))
 
-    return user_mode, user_mean
+    return user_mode, user_mean, user_knn
 
 
 def convert_scores_to_pecentage(scores):
@@ -31,7 +37,7 @@ def calculate_similarities(user_scores, predition_scores):
     for i in range(len(user_scores)):
         if list(user_scores)[i] == list(predition_scores)[i]:
             c += 1
-    return 100 * c / len(user_scores)
+    return c / float(len(user_scores)) * 100
 
 
 def calculate_average_accuracy(accs):
@@ -45,20 +51,22 @@ def get_dataset(paths):
 
 
 def plot_x_candidates(dataset, x=101):
-    p_mode, p_mean = [], []
+    p_mode, p_mean, p_knn = [], [], []
     for i in range(x):
         user, old_user = dataset.get_random_user()
         similars = dataset.get_similar_candidates(user)
         engine = Engine(user=user, candidates=similars)
 
-        user_mode, user_mean = compute_predictions(user, engine)
+        user_mode, user_mean, user_knn = compute_predictions(user, engine)
         p_mode.append(calculate_similarities(old_user.scores.__dict__.values(), user_mode.scores.__dict__.values()))
         p_mean.append(calculate_similarities(old_user.scores.__dict__.values(), user_mean.scores.__dict__.values()))
+        p_knn.append(calculate_similarities(old_user.scores.__dict__.values(), user_knn.scores.__dict__.values()))
 
     plot_data(
         'Prediction-accuracy of 50 traits over 100 candidates (2017 data)',
         [p_mode, "Mode", f'Average mode: {round(calculate_average_accuracy(p_mode), 3)}%'],
         [p_mean, "Mean", f'Average mean: {round(calculate_average_accuracy(p_mean), 3)}%'],
+        [p_knn, "KNN", f'Average knn: {round(calculate_average_accuracy(p_knn), 3)}%'],
         [i * 10 for i in range(11)],
         "Prediction-algorithm accuracy in %",
         "Candidate #",
@@ -67,20 +75,22 @@ def plot_x_candidates(dataset, x=101):
 
 
 def plot_x_traits(dataset):
-    p_mode, p_mean = [], []
+    p_mode, p_mean, p_knn = [], [], []
     for i in range(len(TRAIT_KEYWORDS)):
         user, old_user = dataset.get_random_user(limit=len(TRAIT_KEYWORDS) - i)
         similars = dataset.get_similar_candidates(user)
         engine = Engine(user=user, candidates=similars)
 
-        user_mode, user_mean = compute_predictions(user, engine)
+        user_mode, user_mean, user_knn = compute_predictions(user, engine)
         p_mode.append(calculate_similarities(old_user.scores.__dict__.values(), user_mode.scores.__dict__.values()))
         p_mean.append(calculate_similarities(old_user.scores.__dict__.values(), user_mean.scores.__dict__.values()))
+        p_knn.append(calculate_similarities(old_user.scores.__dict__.values(), user_knn.scores.__dict__.values()))
 
     plot_data(
         'Prediction-accuracy over x amount of undetermined traits (2017 data)',
         [p_mode, "Mode", f'Average mode: {round(calculate_average_accuracy(p_mode), 3)}%'],
         [p_mean, "Mean", f'Average mean: {round(calculate_average_accuracy(p_mean), 3)}%'],
+        [p_knn, "KNN", f'Average knn: {round(calculate_average_accuracy(p_knn), 3)}%'],
         [i * 10 for i in range(11)],
         "Prediction-algorithm accuracy in %",
         "# of pre-determined traits",
@@ -110,12 +120,14 @@ def plot_every_trait(dataset, iterations=101):
         plot.show()
 
 
-def plot_data(title, mode_data, mean_data, yticks, ylabel, xlabel, savefig):
-    plot.plot(mode_data[0], label=mode_data[1])
-    plot.plot(mean_data[0], label=mean_data[1])
+def plot_data(title, mode_data, mean_data, knn_data, yticks, ylabel, xlabel, savefig):
+    plot.plot(mode_data[0], label=mode_data[1], color='purple')
+    plot.plot(mean_data[0], label=mean_data[1], color='blue')
+    plot.plot(knn_data[0], label=knn_data[1], color='orange')
     plot.title(title)
-    plot.text(0, 25, mode_data[2], color='blue')
-    plot.text(0, 20, mean_data[2], color='orange')
+    plot.text(0, 30, knn_data[2], color='orange')
+    plot.text(0, 25, mode_data[2], color='purple')
+    plot.text(0, 20, mean_data[2], color='blue')
     plot.yticks(yticks)
     plot.ylabel(ylabel)
     plot.xlabel(xlabel)
@@ -125,8 +137,57 @@ def plot_data(title, mode_data, mean_data, yticks, ylabel, xlabel, savefig):
     plot.show()
 
 
-if __name__ == '__main__':
+def map_calculated_scores_to_user(predictions, user):
+    for row in predictions:
+        user.scores.__dict__[row[0]] = row[1]
+    return user
+
+
+def run_knn():
     dataset = get_dataset(['data/dataset_2017.csv'])
-    plot_x_candidates(dataset)
-    plot_x_traits(dataset)
+    user, old_user = dataset.get_random_user()
+    similars = dataset.get_similar_candidates(user)
+    empty_traits = get_empty_traits(user)
+    engine = Engine(user=user, candidates=similars)
+    prediction_knn = engine.predict_scores_knn(empty_traits)
+    user = map_calculated_scores_to_user(prediction_knn, user)
+
+def run_mean():
+    dataset = get_dataset(['data/dataset_2017.csv'])
+    user, old_user = dataset.get_random_user()
+    similars = dataset.get_similar_candidates(user)
+    empty_traits = get_empty_traits(user)
+    engine = Engine(user=user, candidates=similars)
+    prediction_scores = []
+    for trait in empty_traits:
+        prediction_scores.append((trait, engine.calculate_mean_score(trait)))
+    user = map_calculated_scores_to_user(prediction_scores, user)
+
+def run_mode():
+    dataset = get_dataset(['data/dataset_2017.csv'])
+    user, old_user = dataset.get_random_user()
+    similars = dataset.get_similar_candidates(user)
+    empty_traits = get_empty_traits(user)
+    engine = Engine(user=user, candidates=similars)
+    prediction_scores = []
+    for trait in empty_traits:
+        prediction_mode, confidence = engine.calculate_score_upcf(trait)
+        prediction_scores.append((trait, prediction_mode))
+    user = map_calculated_scores_to_user(prediction_scores, user)
+
+if __name__ == '__main__':
+    start = timeit.default_timer()
+    run_knn()
+    end = timeit.default_timer()
+    print(f'Time KNN-algorithm: {end-start} seconds')
+    start = timeit.default_timer()
+    run_mean()
+    end = timeit.default_timer()
+    print(f'Time Mean-algorithm: {end-start} seconds')
+    start = timeit.default_timer()
+    run_mode()
+    end = timeit.default_timer()
+    print(f'Time Mode-algorithm: {end-start} seconds')
+    # plot_x_candidates(dataset)
+    # plot_x_traits(dataset)
     # plot_every_trait(dataset)
