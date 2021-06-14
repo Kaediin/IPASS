da404a47e46ca6e9092bc3b6f80c254ce6aa707a
kaediin <skaedin@gmail.com>
2021-06-14 14:46:35 +0200

    cleaned up code and moved utilities function to utils.py.

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import copy as copy
-
 import matplotlib.pyplot as plot
 
-from models import *
 from utils import *
 
 
@@ -23,32 +20,6 @@ def compute_predictions(user, engine):
         user_mean.scores.__dict__[trait] = str(round(prediction_mean))
 
     return user_UPCF, user_mean, user_knn
-
-
-def convert_scores_to_pecentage(scores):
-    percentages = {}
-    total = sum(scores.values())
-    for k, v in scores.items():
-        percentages[k] = 100 * v / total
-    return percentages
-
-
-def calculate_similarities(user_scores, predition_scores):
-    c = 0
-    for i in range(len(user_scores)):
-        if list(user_scores)[i] == list(predition_scores)[i]:
-            c += 1
-    return c / float(len(user_scores)) * 100
-
-
-def calculate_average_accuracy(accs):
-    return sum(accs) / len(accs)
-
-
-def get_dataset(paths):
-    dataset = Dataset(paths)
-    dataset.load()
-    return dataset
 
 
 def plot_x_candidates(dataset, x=101):
@@ -138,63 +109,7 @@ def plot_data(title, UPCF_data, mean_data, knn_data, yticks, ylabel, xlabel, sav
     plot.show()
 
 
-def map_calculated_scores_to_user(predictions, user):
-    for row in predictions:
-        user.scores.__dict__[row[0]] = row[1]
-    return user
-
-
-def run_knn():
-    dataset = Dataset(['data/dataset_2017.csv'])
-    all_candidates = dataset.load()
-    user, old_user = dataset.get_random_user()
-    # similars = dataset.get_similar_candidates(user)
-    empty_traits = get_empty_traits(user)
-    engine = Engine(user=user, candidates=all_candidates)
-    prediction_knn = engine.predict_scores_knn(empty_traits)
-    user = map_calculated_scores_to_user(prediction_knn, user)
-
-
-def run_mean():
-    dataset = Dataset(['data/dataset_2017.csv'])
-    all_candidates = dataset.load()
-    user, old_user = dataset.get_random_user()
-    # similars = dataset.get_similar_candidates(user)
-    empty_traits = get_empty_traits(user)
-    engine = Engine(user=user, candidates=all_candidates)
-    prediction_scores = []
-    for trait in empty_traits:
-        prediction_scores.append((trait, engine.calculate_mean_score(trait)))
-    user = map_calculated_scores_to_user(prediction_scores, user)
-
-
-def run_UPCF():
-    dataset = Dataset(['data/dataset_2017.csv'])
-    all_candidates = dataset.load()
-    user, old_user = dataset.get_random_user()
-    # similars = dataset.get_similar_candidates(user)
-    empty_traits = get_empty_traits(user)
-    engine = Engine(user=user, candidates=all_candidates)
-    prediction_scores = []
-    for trait in empty_traits:
-        prediction_UPCF, confidence = engine.calculate_score_upcf(trait)
-        prediction_scores.append((trait, prediction_UPCF))
-    user = map_calculated_scores_to_user(prediction_scores, user)
-
-
 if __name__ == '__main__':
-    # start = timeit.default_timer()
-    # run_knn()
-    # end = timeit.default_timer()
-    # print(f'Time KNN-algorithm: {end-start} seconds')
-    # start = timeit.default_timer()
-    # run_mean()
-    # end = timeit.default_timer()
-    # print(f'Time Mean-algorithm: {end-start} seconds')
-    # start = timeit.default_timer()
-    # run_UPCF()
-    # end = timeit.default_timer()
-    # print(f'Time UPCF-algorithm: {end-start} seconds')
     dataset = get_dataset(['data/dataset_2017.csv'])
     plot_x_candidates(dataset)
     plot_x_traits(dataset)
--- utils.py
+++ utils.py
@@ -1,6 +1,41 @@
+from models import *
+
+
 def get_empty_traits(user):
     empty_traits = []
     for trait, score in user.scores.__dict__.items():
         if score == 0:
             empty_traits.append(trait)
     return empty_traits
+
+
+def map_calculated_scores_to_user(predictions, user):
+    for row in predictions:
+        user.scores.__dict__[row[0]] = row[1]
+    return user
+
+
+def convert_scores_to_pecentage(scores):
+    percentages = {}
+    total = sum(scores.values())
+    for k, v in scores.items():
+        percentages[k] = 100 * v / total
+    return percentages
+
+
+def calculate_similarities(user_scores, predition_scores):
+    c = 0
+    for i in range(len(user_scores)):
+        if list(user_scores)[i] == list(predition_scores)[i]:
+            c += 1
+    return c / float(len(user_scores)) * 100
+
+
+def calculate_average_accuracy(accs):
+    return sum(accs) / len(accs)
+
+
+def get_dataset(paths):
+    dataset = Dataset(paths)
+    dataset.load()
+    return dataset
