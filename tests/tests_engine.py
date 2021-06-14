7434eee6ebf5449241a5be8144fd256131d3873d
kaediin <skaedin@gmail.com>
2021-06-14 16:05:22 +0200

    Research, Code and document KNN-algorithm #15 Added unittests for KNN

--- tests/tests_engine.py
+++ tests/tests_engine.py
@@ -13,6 +13,13 @@ def get_random_user_engine():
     return engine, user
 
 
+def user_has_no_missing_scores(scores):
+    for k, v in scores.items():
+        if int(v) == 0 or int(v) is None or int(v) < 1 or int(v) > 4:
+            return False
+    return True
+
+
 class TestCases(unittest.TestCase):
 
     def test_user_has_traits(self):
@@ -27,6 +34,27 @@ class TestCases(unittest.TestCase):
             compute_predictions(user, engine)
         except (IndexError, ValueError) as e:
             self.fail(f"Function raised exception: {e.args}")
+
+    def test_knn_has_results(self):
+        # get a random user and engine setup
+        engine, user = get_random_user_engine()
+        # get results for each algorithms and check to see if the results are valid
+        p_UPCF, p_mean, p_knn = compute_predictions(user, engine)
+        self.assertTrue(user_has_no_missing_scores(p_knn.scores.__dict__))
+
+    def test_mean_has_results(self):
+        # get a random user and engine setup
+        engine, user = get_random_user_engine()
+        # get results for each algorithms and check to see if the results are valid
+        p_UPCF, p_mean, p_knn = compute_predictions(user, engine)
+        self.assertTrue(user_has_no_missing_scores(p_mean.scores.__dict__))
+
+    def test_UPCF_has_results(self):
+        # get a random user and engine setup
+        engine, user = get_random_user_engine()
+        # get results for each algorithms and check to see if the results are valid
+        p_UPCF, p_mean, p_knn = compute_predictions(user, engine)
+        self.assertTrue(user_has_no_missing_scores(p_UPCF.scores.__dict__))
 
 
 if __name__ == '__main__':
